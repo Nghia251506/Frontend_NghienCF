e2d7ebe5ae6d34afcb8595b2198268279d01dd48
TNC <89584074+Nghia251506@users.noreply.github.com>
2025-10-02 15:49:59 +0700

    Update axiosClient.tsx

--- src/axios/axiosClient.tsx
+++ src/axios/axiosClient.tsx
@@ -3,7 +3,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  withCredentials: false,
 });
 
 // request interceptor
