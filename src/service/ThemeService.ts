ef91d8771f4af30b70833d26ee553466ef35a3f3
TNC <89584074+Nghia251506@users.noreply.github.com>
2025-10-06 03:19:46 +0700

    Create ThemeService.ts

---
+++ src/service/ThemeService.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+import type { ThemeDto } from "../utils/applyTheme";
+
+const API = import.meta.env.VITE_API_BASE; // https://backend.../api
+export const getTheme = async () => (await axios.get<ThemeDto>(`${API}/theme`)).data;
+export const updateTheme = async (dto: ThemeDto) => (await axios.put(`${API}/theme`, dto)).data;
