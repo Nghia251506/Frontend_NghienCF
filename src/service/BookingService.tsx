89108d6aaaddb656cfa276f03948beb337227155
Nghia251506 <ntn8530@gmail.com>
2025-10-08 02:17:18 +0700

    fix Error 500 booking 1

--- src/service/BookingService.tsx
+++ src/service/BookingService.tsx
@@ -6,9 +6,9 @@ import { Booking, BookingDto, BookingResponseDto, CreateBookingDto } from "../ty
 const BOOKING_URL = "/booking";
 
 export const getAllBooking = async (): Promise<Booking[]> => {
-  return await axiosClient.get(`${BOOKING_URL}/getall`);
+  return await axiosClient.get(`${BOOKING_URL}/getall`, {withCredentials: true});
 };
 
 export const createBooking = async (dto: CreateBookingDto): Promise<BookingResponseDto> => {
-  return await axiosClient.post(`${BOOKING_URL}/create`, dto);
+  return await axiosClient.post(`${BOOKING_URL}/create`, dto, {withCredentials: true});
 };
