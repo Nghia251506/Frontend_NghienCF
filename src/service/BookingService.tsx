bd86ebb739f68a9dc5977b3906d78bcfa565646c
Nghia251506 <ntn8530@gmail.com>
2025-10-08 02:22:10 +0700

    Update Booking

--- src/service/BookingService.tsx
+++ src/service/BookingService.tsx
@@ -6,9 +6,9 @@ import { Booking, BookingDto, BookingResponseDto, CreateBookingDto } from "../ty
 const BOOKING_URL = "/booking";
 
 export const getAllBooking = async (): Promise<Booking[]> => {
-  return await axiosClient.get(`${BOOKING_URL}/getall`, { withCredentials: true });
+  return await axiosClient.get(`${BOOKING_URL}/getall`);
 };
 
 export const createBooking = async (dto: CreateBookingDto): Promise<BookingResponseDto> => {
-  return await axiosClient.post(`${BOOKING_URL}/create`, dto, {withCredentials: true});
+  return await axiosClient.post(`${BOOKING_URL}/create`, dto);
 };
