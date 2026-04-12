036b8a18109040253627487df67be3ef8d2fd58c
Nghia251506 <ntn8530@gmail.com>
2026-04-12 16:13:46 +0700

    Fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,9 +45,9 @@ export default function App() {
     <Router>
       <Routes>
         <Route element={
-            // <MaintenanceGuard>
+            <MaintenanceGuard>
               <Layout />
-            // </MaintenanceGuard>
+            </MaintenanceGuard>
           }>
           <Route path="/" element={<Home />} />
           <Route path="/booking/:showId" element={<Booking />} />
